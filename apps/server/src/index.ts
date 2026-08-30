93cf40bc50d3bded48a64559a0f1cefc02dd959a
thinesjs <thinesjs@gmail.com>
2026-08-30 18:48:59 +0800

    feat(server): export AppRouter type for the web client

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,6 +2,8 @@ import { serve } from "@hono/node-server"
 import { startServer } from "./bootstrap"
 import { loadEnv } from "./env"
 
+export type { AppRouter } from "./routers/index"
+
 startServer(loadEnv(), serve).catch((error: Error) => {
 	console.error(error.message)
 	process.exit(1)
